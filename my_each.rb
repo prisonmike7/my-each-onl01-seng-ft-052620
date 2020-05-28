b1f1ca0782a4e182a3ac6d301e4e738f0bc42c24
Michael Harris <mwharris7@gmail.com>
2020-05-28 09:08:10 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,8 @@
 def my_each(array)
   i = 0
-  while i < array.size
+  while i <= array.size
     yeild(array[i])
     i += 1
   end
   array
-end
-
-collection = [1, 2, 3, 4]
-my_each(collection) do |i|
-  puts i
 end
